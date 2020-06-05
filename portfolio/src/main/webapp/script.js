9ccc6f5f629f55d2e66b7aaca62f5ea760b1cc76
Brakia <brakia.jackson@yahoo.com>
2020-06-05 19:40:41 +0000

    Created portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,19 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+let modal = document.getElementById("myModal");
+let i;
+let img = document.getElementsByClassName("gallery");
+let modalImg = document.getElementById("img01");
+for(i=0;i< img.length;i++) {    
+  img[i].onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+}
+
+let closeSpan = document.getElementsByClassName("close")[0];
+closeSPan.onclick = function() { 
+   modal.style.display = "none";
+}
